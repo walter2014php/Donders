a15e7a7a37154a28fdb7e78db99796f26493f501
walter2014php <122050889+walter2014php@users.noreply.github.com>
2023-01-05 20:22:56 +0100

    Create service.ImportExternLogger.php

---
+++ service.ImportExternLogger.php
@@ -0,0 +1,130 @@
+
+Gesprek geopend. 1 ongelezen bericht.
+
+Spring naar content
+Gmail gebruiken met schermlezers
+1 van 910
+donders
+Inbox
+
+Walter Pothof | Factif BV
+Bijlagen
+20:21 (0 minuten geleden)
+aan mij
+
+   
+Bericht vertalen
+Uitzetten voor: Engels
+ 
+
+7
+ bijlagen
+  • Gescand door Gmail
+
+<?php
+
+    /**
+     * Logger for processing json files to imp_extern_...
+     * Logger for processing imp_extern_... to imp_Stock
+     */
+    class ImportExternLogger
+    {
+        /**
+         * @param string $filename
+         * @return void
+         */
+        public function logStartProcess(string $filename): void
+        {
+            if ($this->isExistingLog($filename)) {
+                $this->updateLog($filename);
+            } else {
+                $this->insertLog($filename);
+            }
+        }
+
+        /**
+         * @param string $latestJsonFile
+         * @return void
+         */
+        public function logEndProcess(string $latestJsonFile): void
+        {
+            $dateArray = $this->getDateInfo();
+
+            $sql = "UPDATE imp_extern_log set `message` = 'Ended processing json file.', `time` = '" . $dateArray['time'] . "',
+                `date` = '" . $dateArray['date'] . "', status = 1 where imp_filename = '" . $latestJsonFile . "'";
+            DatabaseService::Query($sql);
+        }
+
+        /**
+         * @param string $filename
+         * @return void
+         */
+        public function updateLog(string $filename): void
+        {
+            $dateArray = $this->getDateInfo();
+
+            $logQuery = new Query("UPDATE imp_extern_log set `time` = :time, `date` = :date WHERE imp_filenane = :filename");
+            $logQuery->bindArray(['time' => $dateArray['time'], 'date' => $dateArray['date'], 'filename' => $filename]);
+            $logQuery->execute();
+        }
+
+        /**
+         * @param string $filename
+         * @return void
+         */
+        public function insertLog(string $filename): void
+        {
+            $dateArray = $this->getDateInfo();
+
+            $logQuery = new Query("INSERT INTO imp_extern_log (`time`, `date`, imp_filename, `process`, `message`) VALUES (:time, :date, :filename, 'import', 'Started processing json file.')");
+            $logQuery->bindArray(['time' => $dateArray['time'], 'date' => $dateArray['date'], 'filename' => $filename]);
+            $logQuery->execute();
+        }
+
+        /**
+         * @param string $filename
+         * @return bool
+         */
+        private function isExistingLog(string $filename): bool
+        {
+            $logQuery = new Query("SELECT id FROM imp_extern_log  WHERE imp_filename = :filename");
+            $logQuery->bind('filename', $filename);
+            $logQuery->execute();
+
+
+            if ($logQuery->count() > 0) {
+                return true;
+            }
+
+            return false;
+        }
+
+        public function logStartProcessStock(string $uuid)
+        {
+            $this->insertProcessStockLog($uuid);
+        }
+
+        public function insertProcessStockLog(string $uuid)
+        {
+            $dateArray = $this->getDateInfo();
+
+            $sql = new Query("INSERT INTO imp_extern_log (process, uuid, status, message, `time`, `date`) VALUES ('export', '" . $uuid . "', 0, 'Started processing stock.', '".$dateArray['time'] ."', '".$dateArray['date']."')");
+            $sql->execute();
+        }
+
+        public function endProcessStockLog(string $uuid)
+        {
+            $dateArray = $this->getDateInfo();
+
+            $logQuery = new Query("UPDATE imp_extern_log set `time` = :time, `date` = :date, status = 1, message = 'Ended processing stock.' WHERE uuid = :uuid");
+            $logQuery->bindArray(['time' => $dateArray['time'], 'date' => $dateArray['date'], 'uuid' => $uuid]);
+            $logQuery->execute();
+        }
+
+        private function getDateInfo(): array
+        {
+            $time = (new DateTime('now'))->format('h:m:i');
+            $date = (new DateTime('now'))->format('Y-m-d h:m:i');
+            return ['time' => $time, 'date' => $date]  ;
+        }
+    }
